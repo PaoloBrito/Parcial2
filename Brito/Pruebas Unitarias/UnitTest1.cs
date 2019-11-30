62c4c46fbe65494ecdfe963a94e6a37263ae79b4
visual_community@outlook.com <visual_community@outlook.com>
2019-11-30 12:31:12 -0400

    Primer Commit

---
+++ Brito/Pruebas Unitarias/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brito;
+
+
+namespace Pruebas_Unitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PruebaSave()
+        {
+            //Organizar
+            Company comp = new Company("Alfa", 2000);
+            double valoresperado = 2500;
+            //Actuar
+            comp.Save(500);
+            double valorobtenido = comp.SaveProject;
+            //Afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+
+        [TestMethod]
+        public void PruebaSpend()
+        {
+            //Organizar
+            Company comp = new Company("Alfa", 2000);
+            double valoresperado = 1500;
+            //Actuar
+            comp.Spend(500);
+            double valorobtenido = comp.SaveProject;
+            //Afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+
+        [TestMethod]
+        public void PruebaChangeName()
+        {
+            //Organizar
+            Company comp = new Company("Alfa", 2000);
+            double valoresperado = 2500;
+            //Actuar
+            comp.ChangeName("Beta");
+            double valorobtenido = comp.SaveProject;
+            //Afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+
+
+
+    }
+}
